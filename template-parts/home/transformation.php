7027c206fda467e3d79d3f92e98e8600189a4d3d
Jaweria Shoukat <jaweeriashoukat@gmail.com>
2026-09-24 20:07:14 +0500

    Card

--- template-parts/home/transformation.php
+++ template-parts/home/transformation.php
@@ -5,150 +5,150 @@
         </h2>
     </div>
     <div class="container mx-auto px-4 grid md:grid-cols-3 grid-cols-1 gap-5">
-        <div class="relative w-full h-[300px] sm:h-[300px] lg:h-[350px] overflow-hidden rounded-[30px] bg-[#d9dde0]">
+        <div class="relative w-full h-[300px] sm:h-[300px] lg:h-[450px] overflow-hidden rounded-[30px] bg-[#d9dde0]">
             <!-- Image -->
             <img src="<?php echo get_template_directory_uri(); ?>/assets/images/image-435-1.webp"
                 alt="Same-Day Dentistry" class="absolute inset-0 w-full h-full object-cover object-center">
 
             <!-- Glass Card -->
             <div
-                class="absolute left-[6%] right-[6%] bottom-[8%] min-h-[240px] lg:min-h-[288px] flex flex-col items-center justify-center rounded-[27px] border border-white/50 bg-white/[0.62] backdrop-blur-[14px] shadow-[0_12px_35px_rgba(0,0,0,0.18)] px-5 py-7 md:px-10">
+                class="absolute left-3 right-3 !bottom-3 flex flex-col items-center justify-center rounded-[27px] border border-white/50 bg-white/10 backdrop-blur-[14px] shadow-[0_12px_35px_rgba(0,0,0,0.18)] px-5 py-5">
 
-                <h3 class="text-lg sm:text-2xl text-coff_black text-center">
+                <h3 class="text-lg text-coff_black text-center">
                     Same-Day Dentistry
                 </h3>
 
-                <p class="text-base text-secondaryLight text-center">
+                <p class="text-sm text-secondaryLight text-center">
                     Same-day dentistry represents a significant advancement in
                     dental efficiency, utilising CAD/CAM tech
                 </p>
 
-                <a href="#" class="mt-6 lg:mt-7 btn-consult">
+                <a href="#" class="mt-3 btn-consult">
                     See Details
                 </a>
 
             </div>
 
         </div>
-        <div class="relative w-full h-[300px] sm:h-[300px] lg:h-[350px] overflow-hidden rounded-[30px] bg-[#d9dde0]">
+          <div class="relative w-full h-[300px] sm:h-[300px] lg:h-[450px] overflow-hidden rounded-[30px] bg-[#d9dde0]">
             <!-- Image -->
             <img src="<?php echo get_template_directory_uri(); ?>/assets/images/image-435-1.webp"
                 alt="Same-Day Dentistry" class="absolute inset-0 w-full h-full object-cover object-center">
 
             <!-- Glass Card -->
             <div
-                class="absolute left-[6%] right-[6%] bottom-[8%] min-h-[240px] lg:min-h-[288px] flex flex-col items-center justify-center rounded-[27px] border border-white/50 bg-white/[0.62] backdrop-blur-[14px] shadow-[0_12px_35px_rgba(0,0,0,0.18)] px-5 py-7 md:px-10">
+                class="absolute left-3 right-3 !bottom-3 flex flex-col items-center justify-center rounded-[27px] border border-white/50 bg-white/10 backdrop-blur-[14px] shadow-[0_12px_35px_rgba(0,0,0,0.18)] px-5 py-5">
 
-                <h3 class="text-lg sm:text-2xl text-coff_black text-center">
+                <h3 class="text-lg text-coff_black text-center">
                     Same-Day Dentistry
                 </h3>
 
-                <p class="text-base text-secondaryLight text-center">
+                <p class="text-sm text-secondaryLight text-center">
                     Same-day dentistry represents a significant advancement in
                     dental efficiency, utilising CAD/CAM tech
                 </p>
 
-                <a href="#" class="mt-6 lg:mt-7 btn-consult">
+                <a href="#" class="mt-3 btn-consult">
                     See Details
                 </a>
 
             </div>
 
         </div>
-        <div class="relative w-full h-[300px] sm:h-[300px] lg:h-[350px] overflow-hidden rounded-[30px] bg-[#d9dde0]">
+          <div class="relative w-full h-[300px] sm:h-[300px] lg:h-[450px] overflow-hidden rounded-[30px] bg-[#d9dde0]">
             <!-- Image -->
             <img src="<?php echo get_template_directory_uri(); ?>/assets/images/image-435-1.webp"
                 alt="Same-Day Dentistry" class="absolute inset-0 w-full h-full object-cover object-center">
 
             <!-- Glass Card -->
             <div
-                class="absolute left-[6%] right-[6%] bottom-[8%] min-h-[240px] lg:min-h-[288px] flex flex-col items-center justify-center rounded-[27px] border border-white/50 bg-white/[0.62] backdrop-blur-[14px] shadow-[0_12px_35px_rgba(0,0,0,0.18)] px-5 py-7 md:px-10">
+                class="absolute left-3 right-3 !bottom-3 flex flex-col items-center justify-center rounded-[27px] border border-white/50 bg-white/10 backdrop-blur-[14px] shadow-[0_12px_35px_rgba(0,0,0,0.18)] px-5 py-5">
 
-                <h3 class="text-lg sm:text-2xl text-coff_black text-center">
+                <h3 class="text-lg text-coff_black text-center">
                     Same-Day Dentistry
                 </h3>
 
-                <p class="text-base text-secondaryLight text-center">
+                <p class="text-sm text-secondaryLight text-center">
                     Same-day dentistry represents a significant advancement in
                     dental efficiency, utilising CAD/CAM tech
                 </p>
 
-                <a href="#" class="mt-6 lg:mt-7 btn-consult">
+                <a href="#" class="mt-3 btn-consult">
                     See Details
                 </a>
 
             </div>
 
         </div>
-        <div class="relative w-full h-[300px] sm:h-[300px] lg:h-[350px] overflow-hidden rounded-[30px] bg-[#d9dde0]">
+          <div class="relative w-full h-[300px] sm:h-[300px] lg:h-[450px] overflow-hidden rounded-[30px] bg-[#d9dde0]">
             <!-- Image -->
             <img src="<?php echo get_template_directory_uri(); ?>/assets/images/image-435-1.webp"
                 alt="Same-Day Dentistry" class="absolute inset-0 w-full h-full object-cover object-center">
 
             <!-- Glass Card -->
             <div
-                class="absolute left-[6%] right-[6%] bottom-[8%] min-h-[240px] lg:min-h-[288px] flex flex-col items-center justify-center rounded-[27px] border border-white/50 bg-white/[0.62] backdrop-blur-[14px] shadow-[0_12px_35px_rgba(0,0,0,0.18)] px-5 py-7 md:px-10">
+                class="absolute left-3 right-3 !bottom-3 flex flex-col items-center justify-center rounded-[27px] border border-white/50 bg-white/10 backdrop-blur-[14px] shadow-[0_12px_35px_rgba(0,0,0,0.18)] px-5 py-5">
 
-                <h3 class="text-lg sm:text-2xl text-coff_black text-center">
+                <h3 class="text-lg text-coff_black text-center">
                     Same-Day Dentistry
                 </h3>
 
-                <p class="text-base text-secondaryLight text-center">
+                <p class="text-sm text-secondaryLight text-center">
                     Same-day dentistry represents a significant advancement in
                     dental efficiency, utilising CAD/CAM tech
                 </p>
 
-                <a href="#" class="mt-6 lg:mt-7 btn-consult">
+                <a href="#" class="mt-3 btn-consult">
                     See Details
                 </a>
 
             </div>
 
         </div>
-        <div class="relative w-full h-[300px] sm:h-[300px] lg:h-[350px] overflow-hidden rounded-[30px] bg-[#d9dde0]">
+          <div class="relative w-full h-[300px] sm:h-[300px] lg:h-[450px] overflow-hidden rounded-[30px] bg-[#d9dde0]">
             <!-- Image -->
             <img src="<?php echo get_template_directory_uri(); ?>/assets/images/image-435-1.webp"
                 alt="Same-Day Dentistry" class="absolute inset-0 w-full h-full object-cover object-center">
 
             <!-- Glass Card -->
             <div
-                class="absolute left-[6%] right-[6%] bottom-[8%] min-h-[240px] lg:min-h-[288px] flex flex-col items-center justify-center rounded-[27px] border border-white/50 bg-white/[0.62] backdrop-blur-[14px] shadow-[0_12px_35px_rgba(0,0,0,0.18)] px-5 py-7 md:px-10">
+                class="absolute left-3 right-3 !bottom-3 flex flex-col items-center justify-center rounded-[27px] border border-white/50 bg-white/10 backdrop-blur-[14px] shadow-[0_12px_35px_rgba(0,0,0,0.18)] px-5 py-5">
 
-                <h3 class="text-lg sm:text-2xl text-coff_black text-center">
+                <h3 class="text-lg text-coff_black text-center">
                     Same-Day Dentistry
                 </h3>
 
-                <p class="text-base text-secondaryLight text-center">
+                <p class="text-sm text-secondaryLight text-center">
                     Same-day dentistry represents a significant advancement in
                     dental efficiency, utilising CAD/CAM tech
                 </p>
 
-                <a href="#" class="mt-6 lg:mt-7 btn-consult">
+                <a href="#" class="mt-3 btn-consult">
                     See Details
                 </a>
 
             </div>
 
         </div>
-        <div class="relative w-full h-[300px] sm:h-[300px] lg:h-[350px] overflow-hidden rounded-[30px] bg-[#d9dde0]">
+          <div class="relative w-full h-[300px] sm:h-[300px] lg:h-[450px] overflow-hidden rounded-[30px] bg-[#d9dde0]">
             <!-- Image -->
             <img src="<?php echo get_template_directory_uri(); ?>/assets/images/image-435-1.webp"
                 alt="Same-Day Dentistry" class="absolute inset-0 w-full h-full object-cover object-center">
 
             <!-- Glass Card -->
             <div
-                class="absolute left-[6%] right-[6%] bottom-[8%] min-h-[240px] lg:min-h-[288px] flex flex-col items-center justify-center rounded-[27px] border border-white/50 bg-white/[0.62] backdrop-blur-[14px] shadow-[0_12px_35px_rgba(0,0,0,0.18)] px-5 py-7 md:px-10">
+                class="absolute left-3 right-3 !bottom-3 flex flex-col items-center justify-center rounded-[27px] border border-white/50 bg-white/10 backdrop-blur-[14px] shadow-[0_12px_35px_rgba(0,0,0,0.18)] px-5 py-5">
 
-                <h3 class="text-lg sm:text-2xl text-coff_black text-center">
+                <h3 class="text-lg text-coff_black text-center">
                     Same-Day Dentistry
                 </h3>
 
-                <p class="text-base text-secondaryLight text-center">
+                <p class="text-sm text-secondaryLight text-center">
                     Same-day dentistry represents a significant advancement in
                     dental efficiency, utilising CAD/CAM tech
                 </p>
 
-                <a href="#" class="mt-6 lg:mt-7 btn-consult">
+                <a href="#" class="mt-3 btn-consult">
                     See Details
                 </a>
 
